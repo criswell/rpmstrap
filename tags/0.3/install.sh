622c7c2fa6a36d9c3c5909fbe9430caa06b3bac1
sam <devnull@localhost>
2005-07-28 15:24:13 -0400

    [svn] Tagging the 0.3 release of rpmstrap

---
+++ tags/0.3/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BINDIR=/usr/bin
+LIBDIR=/usr/lib/rpmstrap
+DOCDIR=/usr/share/doc/rpmstrap-0.5
+
+cp -fr rpmstrap $BINDIR/.
+mkdir -p $LIBDIR
+cp -fr lib/functions $LIBDIR/.
+cp -fr lib/scripts $LIBDIR/.
+mkdir -p $DOCDIR
+cp -fr lib/*.txt $DOCDIR/.
+cp -fr LICENSE $DOCDIR/.
+cp -fr README $DOCDIR/.
+cp -fr TODO $DOCDIR/.
+
+echo rpmstrap installed
