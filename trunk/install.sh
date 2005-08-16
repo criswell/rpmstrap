e62ddecd63baa1196614359a7d279e566a42de90
sam <devnull@localhost>
2005-08-16 11:16:50 -0400

    [svn] Make install script a bit less stupid.

--- trunk/install.sh
+++ trunk/install.sh
@@ -4,6 +4,33 @@ BINDIR=/usr/bin
 LIBDIR=/usr/lib/rpmstrap
 DOCDIR=/usr/share/doc/rpmstrap-0.5
 
+if [ $# != 0 ] ; then
+    while true ; do
+        case "$1" in
+            -h)
+                echo "install.sh"
+                echo "----------"
+                echo "Run with no arguments to install software"
+                echo "Run with '-p' to purge/remove old software"
+                exit 0
+                ;;
+            -p)
+                rm -f $BINDIR/rpmstrap
+                rm -f $BINDIR/rpm-solver.py
+                rm -fr $LIBDIR
+                rm -fr $DOCDIR
+                echo "rpmstrap purged"
+                exit 0
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+fi
+
+# Default is to install
+
 cp -fr rpmstrap $BINDIR/.
 cp -fr tools/rpm-solver.py $BINDIR/.
 mkdir -p $LIBDIR
